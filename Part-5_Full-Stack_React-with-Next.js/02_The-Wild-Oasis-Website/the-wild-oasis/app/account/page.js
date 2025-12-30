b04736a125ecea192eb9a7ddf28b597ee35bcfd9
Mohit Kucheriya <kucheriyamohit@gmail.com>
2025-12-30 13:47:07 +0530

    Static part of 'The Wild Oasis' completed

--- Part-5_Full-Stack_React-with-Next.js/02_The-Wild-Oasis-Website/the-wild-oasis/app/account/page.js
+++ Part-5_Full-Stack_React-with-Next.js/02_The-Wild-Oasis-Website/the-wild-oasis/app/account/page.js
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: "Account",
+};
+
 export default function page() {
-  return <h1>This is the Account page</h1>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Mohit
+    </h2>
+  );
 }
