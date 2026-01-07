158a752f1747a2a1741dc5c7668c126d7933d80c
Mohit Kucheriya <kucheriyamohit@gmail.com>
2026-01-07 18:50:35 +0530

    Sharing State Between Client and Server: The URL

--- Part-5_Full-Stack_React-with-Next.js/02_The-Wild-Oasis-Website/the-wild-oasis/app/cabins/page.js
+++ Part-5_Full-Stack_React-with-Next.js/02_The-Wild-Oasis-Website/the-wild-oasis/app/cabins/page.js
@@ -1,19 +1,23 @@
 import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "../_components/Filter";
 
 // All of this here is at route level
 // Next.js will revalidate this page on every request (to always show the latest data)
 // export const revalidate = 0;
 
 // If we want to revalidate this page at a specific interval (e.g., every 10 seconds), we can use the following line instead which is called Incremental Static Regeneration (ISR):
-export const revalidate = 3600; // Revalidate every hour
+export const revalidate = 3600; // Revalidate every hour i.e. it only applies to statically generated pages.
+// Now this revalidate will not work because we are using dynamic data (searchParams) in this page.
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -28,8 +32,12 @@ export default function Page() {
         to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8 ">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
